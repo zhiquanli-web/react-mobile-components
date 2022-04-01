fc1937493f396fd620f42a6c4c7511fa2d7fe78d
lizhiquan <lizhiquan@tuzhanai.com>
2022-04-01 19:36:34 +0800

    全局scss方法

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
-import './assets/styles/index.scss'
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HashRouter>
     <App />
